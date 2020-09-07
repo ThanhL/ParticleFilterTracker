d80a7b603b54fbe3fb669b68fae81545724b47ec
unknown <thanhl354@gmail.com>
2020-09-07 20:11:22 +1000

    separted opencv yolo into wrapper class and added basic process frame function

---
+++ YOLO_CV_Wrapper.py
@@ -0,0 +1,99 @@
+import cv2
+import numpy as np
+np.random.seed(seed=501)
+
+class YOLO_CV_Wrapper():
+    def __init__(self, yolo_weights, yolo_cfg, yolo_classes, gpu_enabled=False, confidence_thresh=0.5,
+                nms_thresh=0.3):
+        ### Initialize OpenCV's Darknet
+        self.yolo_weights = yolo_weights
+        self.yolo_cfg = yolo_cfg
+        self.yolo_classes = open(yolo_classes).read().strip().split('\n')
+        self.confidence_thresh = confidence_thresh
+        self.nms_thresh = nms_thresh
+        self.color = np.random.randint(0, 255, size=(len(self.yolo_classes), 3), dtype='uint8')
+
+        ## Darknet creation
+        self.net = cv2.dnn.readNetFromDarknet(yolo_cfg, yolo_weights)
+        
+        ln = self.net.getLayerNames()
+        self.ln = [ln[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
+
+        ## To GPU or not to GPU
+        if gpu_enabled:
+            self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+            self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+    def darknet_process_frame(self, frame):
+        """
+        Take in an opencv frame and runs yolo on the frame. Bounding boxes are drawn
+        onto the frame.
+        """
+        ### Extract frame info
+        (h, w) = frame.shape[:2]
+
+        ### Create a blob and pass the blob through the network
+        blob = cv2.dnn.blobFromImage(frame, 1/255.0, (416, 416), swapRB=True, crop=False)
+        self.net.setInput(blob)
+        outputs = self.net.forward()
+
+        # combine the YOLO's 3 output groups into 1 (10647, 85)
+        # large objects (507, 85)
+        # medium objects (2028, 85)
+        # small objects (8112, 85)
+        outputs = np.vstack(outputs)
+
+
+        # bounding boxes, confidences, class ids
+        boxes = []
+        confidences = []
+        classIDs = []
+
+        ### Iterate through output and apply bounding boxes
+        for output in outputs:
+            scores = output[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+
+            if confidence > self.confidence_thresh:
+                if self.yolo_classes[classID] == "car":
+                    print(self.yolo_classes[classID])
+
+                    box = output[:4] * np.array([w, h, w, h])
+                    (centerX, centerY, width, height) = box.astype("int")
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
+                
+                    # cv2.rectangle(frame, (x, y), (x + width, y+height),
+                    #     (255,255,255), 2)
+
+                    boxes.append([x, y, int(width), int(height)])
+                    confidences.append(float(confidence))
+                    classIDs.append(classID)
+
+        ### Apply Non-maxima supression
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, self.confidence_thresh, self.nms_thresh)
+        
+
+         # ensure at least one detection exists
+        if len(idxs) > 0:
+            # loop over the indexes we are keeping
+            for i in idxs.flatten():
+                # extract the bounding box coordinates
+                (x, y) = (boxes[i][0], boxes[i][1])
+                (w, h) = (boxes[i][2], boxes[i][3])
+                # draw a bounding box rectangle and label on the image
+                color = [int(c) for c in self.color[classIDs[i]]]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                text = "{}: {:.4f}".format(self.yolo_classes[classIDs[i]], confidences[i])
+                cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, color, 2)   
+
+        return frame
+
+
+
+
+
+
+
--- pf_driver.py
+++ pf_driver.py
@@ -2,17 +2,16 @@ import cv2
 import time
 import argparse
 import numpy as np
-import imutils
+from YOLO_CV_Wrapper import YOLO_CV_Wrapper
 
-DEFAULT_VIDEO = "./datasets/volkswagen_pikes_peak_original_cut_3.mp4"
+DEFAULT_VIDEO = "./datasets/single_white_car.mp4"
 YOLOV3_SPP_WEIGHTS = "./models/yolov3/yolov3-spp.weights"
 YOLOV3_SPP_CFG = "./models/yolov3/yolov3-spp.cfg"
 YOLOV3_WEIGHTS = "./models/yolov3/yolov3.weights"
 YOLOV3_CFG = "./models/yolov3/yolov3.cfg"
-CLASSES = open('models/yolov3/coco.names').read().strip().split('\n')
-conf = 0.5
+COCO_CLASSES_TXT = "./models/yolov3/coco.names"
+YOLO_CONFIDENCE_THRESH = 0.5
 
-colors = np.random.randint(0, 255, size=(len(CLASSES), 3), dtype='uint8')
 
 ### YOLO utility funcs
 
@@ -34,17 +33,12 @@ def main():
     gpu_enable = args.gpu
 
 
-    ### Load model
-    ## YOLO Model
-    net = cv2.dnn.readNetFromDarknet(YOLOV3_CFG, YOLOV3_WEIGHTS)
-    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-    ln = net.getLayerNames()
-    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
-    # Use the GPU
-    if gpu_enable:  
-        net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-        net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+    ### YOLO Wrapper Creation
+    yolo_cv = YOLO_CV_Wrapper(yolo_weights=YOLOV3_WEIGHTS,
+                            yolo_cfg=YOLOV3_CFG,
+                            yolo_classes=COCO_CLASSES_TXT,
+                            gpu_enabled=gpu_enable,
+                            confidence_thresh=YOLO_CONFIDENCE_THRESH)
 
 
     ### Run the tracker
@@ -55,52 +49,8 @@ def main():
         # Capture frame-by-frame
         ret, frame = cap.read()
 
-
-        # resize the frame, grab the frame dimensions, and convert it to
-        # a blob
-        (h, w) = frame.shape[:2]
-        H, W = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(frame, 1/255.0, (416, 416), swapRB=True, crop=False)
-
-        # pass the blob through the network and obtain the detections and
-        # predictions
-        net.setInput(blob)
-        outputs = net.forward(ln)
-
-        # combine the 3 output groups into 1 (10647, 85)
-        # large objects (507, 85)
-        # medium objects (2028, 85)
-        # small objects (8112, 85)
-        outputs = np.vstack(outputs)
-
-
-        # loop over each of the layer outputs
-        for output in outputs:
-            scores = output[5:]
-            classID = np.argmax(scores)
-            confidence = scores[classID]
-            if confidence > conf:
-                if CLASSES[classID] == "car":
-                    print(CLASSES[classID])
-                    box = output[:4] * np.array([w, h, w, h])
-                    (centerX, centerY, width, height) = box.astype("int")
-                    x = int(centerX - (width / 2))
-                    y = int(centerY - (height / 2))
-                
-                    cv2.rectangle(frame, (x, y), (x + width, y+height),
-                        (255,255,255), 2)
-
-                # color = [int(c) for c in colors[classID]]
-                # cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-                # text = "{}: {:.4f}".format(CLASSES[classID], confidence)
-                # cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
-
-                # p0 = int(x - w//2), int(y - h//2)
-                # p1 = int(x + w//2), int(y + h//2)
-                # boxes.append([*p0, int(w), int(h)])
-                # confidences.append(float(confidence))
-                # classIDs.append(classID)
-
+        # Pass frame through yolo
+        frame = yolo_cv.darknet_process_frame(frame)
 
         # Display the resulting frame
         cv2.imshow('frame',frame)
